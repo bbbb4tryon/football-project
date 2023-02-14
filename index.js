c25245f2f0979da7caae9fb5264cd8e1fb6922a8
JonahPeck <PeckJonahC@gmail.com>
2023-02-14 14:26:20 -0700

    added team standings functionality

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let leagueList = document.querySelector("#individual-leagues");
 let leagueImage = document.querySelector("#league-logo");
 let leagueName = document.querySelector("#league-name");
 let leagueCountry = document.querySelector("#league-country");
-let leagueStandings = document.querySelector("#league-standings");
+let leagueStandings = document.querySelector("#league-name");
 // let leaguePromo = document.querySelector("#league-
 
 
@@ -13,38 +13,49 @@ let leagueStandings = document.querySelector("#league-standings");
 fetch("http://localhost:3000/leagues")
 .then(response => response.json())
 .then(leagueData => {
-
     //Our wishlist for the code
     addLeagues(leagueData)
     setLeague(leagueData)
+    displayStandings()
     addComment()
     mouseOver()
     mouseOut()
+    
+    // displayLeagueStandings()
     //donationForm()
     //randomizeButton()
 })
 function addLeagues(leagueData) {
     leagueData.forEach(league => {
-        let leagueItem = document.createElement("li");
+        let leagueItem = document.createElement("ol");
         leagueItem.textContent = league.name;
-        
         leagueList.appendChild(leagueItem);
         leagueItem.addEventListener("click", () => {
             setLeague(league)
-            
-
-
         })
+    })     
 //adding a mouseover event to change from home to away colors for the logo
-    });
+
 }
+function displayStandings(leagueData) {
+    leagueStandings.forEach(team => {
+        let newStandings = document.createElement("ol");
+        newStandings.textContent = team.teamStandings;
+        leagueStandings.appendChild(newStandings);
+        // newStandings.addEventListener("click", () => {
+        //     setLeague(team)
+        // })
+    
+    })
+}
+
+
 function setLeague(nextLeague) {
  currentLeague = nextLeague;
 
  leagueName.textContent = nextLeague.name;
  leagueCountry.textContent = nextLeague.country;
  leagueImage.src = nextLeague.leagueLogo;
-
  leagueStandings.textContent = nextLeague.teamStandings;
 
 }
@@ -63,4 +74,28 @@ function addComment() {
         addLeagues(newCommentAdded)
     })
 }
+// function displayLeagueStandings (leagueData) {
+//     for (const teamStandings in leagues)
+
+//     console.log(leagues[teamStandings].teamStandings)
+// }
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 
