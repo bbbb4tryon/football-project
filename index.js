4ca9569d0a7ee96bca927f11a4d944bd61a97778
JonahPeck <PeckJonahC@gmail.com>
2023-02-15 15:35:28 -0700

    functioning additions

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 //code for use to get JS to show up on website
+let leagueItem = document.createElement("li");
+    
 //declare global varibales
+
+
 let currentLeague;
 let leagueList = document.querySelector("#individual-leagues");
 let leagueImage = document.querySelector("#league-logo");
 let leagueName = document.querySelector("#league-name");
 let leagueCountry = document.querySelector("#league-country");
-let leagueStandings = document.querySelector("#league-standings");
+let leagueStandings1 = document.querySelector("#team-1");
+let leagueStandings2 = document.querySelector("#team-2");
+let leagueStandings3 = document.querySelector("#team-3");
+let leagueStandings4 = document.querySelector("#team-4");
+let leagueStandings5 = document.querySelector("#team-5");
+let leagueStandings6 = document.querySelector("#team-6");
+let leagueStandings7 = document.querySelector("#team-7");
+let leagueStandings8 = document.querySelector("#team-8");
+let leagueStandings9 = document.querySelector("#team-9");
+let leagueStandings10 = document.querySelector("#team-10");
+let leagueStandings11 = document.querySelector("#team-11");
+let leagueStandings12 = document.querySelector("#team-12");
+let leagueStandings13 = document.querySelector("#team-13");
+let leagueStandings14 = document.querySelector("#team-14");
+let leagueStandings15 = document.querySelector("#team-15");
+let leagueStandings16 = document.querySelector("#team-16");
+let leagueStandings17 = document.querySelector("#team-17");
+let leagueStandings18 = document.querySelector("#team-18");
+let leagueStandings19 = document.querySelector("#team-19");
+let leagueStandings20 = document.querySelector("#team-20");
+
+
+
+
+
+
+
+
+
+
 let teamLeagueList = document.querySelector("#individual-teams");
+let mouseOver = document.getElementById("title");
+let mouseOut = document.getElementById("title");
 let firstPlace = document.querySelector("#team-1");
 let secondPlace = document.querySelector("#team-2");
 let thirdPlace = document.querySelector("#team-3");
@@ -19,61 +54,46 @@ let ninthPlace = document.querySelector("#team-9");
 let tenthPlace = document.querySelector("#team-10");
 // let leaguePromo = document.querySelector("#league-
 
+// for(let i = 1; i <= 20; i++) {
+//     let team = "team" + i
+//     let teamItems = document.createElement("li");
+//     // console.log(league[team])   
+//     teamItems.textContent = obj[team];
+//     leagueListNew.append(teamItems);
+// }
 
 
 fetch("http://localhost:3000/leagues")
 .then(response => response.json())
-.then(leagueData => {
-    //Our wishlist for the code
-    addLeagues(leagueData)
-    setLeague(leagueData)
+.then(leagueData => { 
+    
 
-    // displayStandings(leagueData)
+    //Our wishlist for the code
+   
+    displayInformation(leagueData);
     addComment()
-    mouseOver()
-    mouseOut()
+    setLeague(leagueData)
+    // mouseOverFunction()
+    // mouseOutFunction()
     
     // displayLeagueStandings()
-    //donationForm()
-    //randomizeButton()
+   
 
 })
-function addLeagues(leagueData) {
-    leagueData.forEach(league => {
-        let leagueItem = document.createElement("li");
-        leagueItem.textContent = league.name;
 
-        teamLeagueList.append(leagueItem);
-        leagueItem.addEventListener("click", () => {
-            
-            for(let i = 1; i <= 20; i++) {
-                let teamItems = document.createElement("li");
-                let team = "team" + i
-                console.log(league[team])   
-                teamItems.textContent = league[team];
-                leagueList.append(teamItems);
-                
-            }
+function displayInformation(leagueData) {
+  let displayLeagueInformation = document.querySelector("#individual-leagues");  
+  leagueData.forEach(league => {
+    let newFootballLeague = document.createElement("li");
+    newFootballLeague.textContent = league.name;
+    displayLeagueInformation.append(newFootballLeague);
 
-            setLeague(league)
-            
-
-
-        })
-        
-    })     
-//adding a mouseover event to change from home to away colors for the logo
-
+    newFootballLeague.addEventListener("click", () => {
+        setLeague(league)
+    })
+  })
 }
-// function displayStandings(leagueData) {
-//     leagueData.forEach(team => {
-        
-//         // newStandings.addEventListener("click", () => {
-//         //     setLeague(team)
-//         // })
-    
-//     })
-// }
+
 
 
 function setLeague(nextLeague) {
@@ -82,8 +102,27 @@ function setLeague(nextLeague) {
  leagueName.textContent = nextLeague.name;
  leagueCountry.textContent = nextLeague.country;
  leagueImage.src = nextLeague.leagueLogo;
-
- leagueStandings.textContent = nextLeague.teamStandings;
+ leagueStandings1.textContent = nextLeague.team1;
+ leagueStandings2.textContent = nextLeague.team2;
+ leagueStandings3.textContent = nextLeague.team3;
+ leagueStandings4.textContent = nextLeague.team4;
+ leagueStandings5.textContent = nextLeague.team5;
+ leagueStandings6.textContent = nextLeague.team6;
+ leagueStandings7.textContent = nextLeague.team7;
+ leagueStandings8.textContent = nextLeague.team8;
+ leagueStandings9.textContent = nextLeague.team9;
+ leagueStandings10.textContent = nextLeague.team10;
+ leagueStandings11.textContent = nextLeague.team11;
+ leagueStandings12.textContent = nextLeague.team12;
+ leagueStandings13.textContent = nextLeague.team13;
+ leagueStandings14.textContent = nextLeague.team14;
+ leagueStandings15.textContent = nextLeague.team15;
+ leagueStandings16.textContent = nextLeague.team16;
+ leagueStandings17.textContent = nextLeague.team17;
+ leagueStandings18.textContent = nextLeague.team18;
+ leagueStandings19.textContent = nextLeague.team19;
+ leagueStandings20.textContent = nextLeague.team20;
+ 
 
 }
 function addComment() {
@@ -99,6 +138,18 @@ function addComment() {
         newComment.appendChild(newSoccerComment);
 
         addLeagues(newCommentAdded)
+    })
+}
+function mouseOverFunction(){
+    mouseOver.addEventListener("mouseover", () => {
+        document.getElementById("title").style.color = "red"
+        let playAnthem = document.getElementById("controls")
+
+    })
+}
+function mouseOutFunction(){
+    mouseOut.addEventListener("mouseout", () => {
+        document.getElementById("title").style.color = "black"
     })
 }
 // function displayLeagueStandings (leagueData) {
