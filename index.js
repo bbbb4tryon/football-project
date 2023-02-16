555ec9d7360b3bc653b30575abc72905f2985ab7
JonahPeck <PeckJonahC@gmail.com>
2023-02-16 13:25:51 -0700

    functioning site

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //code for use to get JS to show up on website
 let leagueItem = document.createElement("li");
-    
+
 //declare global varibales
 
-
+let mouseOut = document.querySelector("#title");
+let mouseOver = document.querySelector("#title");
 let currentLeague;
 let leagueFlag = document.querySelector("#league-flag");
 let leagueList = document.querySelector("#individual-leagues");
@@ -45,67 +46,76 @@ let oldComments = document.querySelector("#comment_new")
 
 
 fetch("http://localhost:3000/leagues")
-.then(response => response.json())
-.then(leagueData => { 
-    
+    .then(response => response.json())
+    .then(leagueData => {
 
-    //Our wishlist for the code
-   
-    displayInformation(leagueData);
-    addComment()
-    setLeague(leagueData)
-    mouseOverFunction()
-    mouseOutFunction()
-    
-    // displayLeagueStandings()
-   
 
-})
+        //Our wishlist for the code
+
+        displayInformation(leagueData);
+        addComment()
+        setLeague(leagueData[0])
+        mouseOverFunction()
+        mouseOutFunction()
+        promoVideo(leagueData)
+
+        // displayLeagueStandings()
+
+
+    })
 
 function displayInformation(leagueData) {
-  let displayLeagueInformation = document.querySelector("#individual-leagues");  
-  leagueData.forEach(league => {
-    let newFootballLeague = document.createElement("li");
-    newFootballLeague.textContent = league.name;
-    displayLeagueInformation.append(newFootballLeague);
+    let displayLeagueInformation = document.querySelector(".card");
+    leagueData.forEach(league => {
+        let newFootballLeague = document.createElement("article");
+        newFootballLeague.textContent = league.name;
+        displayLeagueInformation.append(newFootballLeague);
 
-    newFootballLeague.addEventListener("click", () => {
-        setLeague(league)
+        newFootballLeague.addEventListener("click", () => {
+            setLeague(league)
+        })
     })
-  })
 }
 
 
 
 function setLeague(nextLeague) {
- currentLeague = nextLeague;
+    currentLeague = nextLeague;
 
- leagueName.textContent = nextLeague.name;
- leagueCountry.textContent = nextLeague.country;
- leagueImage.src = nextLeague.leagueLogo;
- leagueFlag.src = nextLeague.flag;
- leagueStandings1.textContent = nextLeague.team1;
- leagueStandings2.textContent = nextLeague.team2;
- leagueStandings3.textContent = nextLeague.team3;
- leagueStandings4.textContent = nextLeague.team4;
- leagueStandings5.textContent = nextLeague.team5;
- leagueStandings6.textContent = nextLeague.team6;
- leagueStandings7.textContent = nextLeague.team7;
- leagueStandings8.textContent = nextLeague.team8;
- leagueStandings9.textContent = nextLeague.team9;
- leagueStandings10.textContent = nextLeague.team10;
- leagueStandings11.textContent = nextLeague.team11;
- leagueStandings12.textContent = nextLeague.team12;
- leagueStandings13.textContent = nextLeague.team13;
- leagueStandings14.textContent = nextLeague.team14;
- leagueStandings15.textContent = nextLeague.team15;
- leagueStandings16.textContent = nextLeague.team16;
- leagueStandings17.textContent = nextLeague.team17;
- leagueStandings18.textContent = nextLeague.team18;
- leagueStandings19.textContent = nextLeague.team19;
- leagueStandings20.textContent = nextLeague.team20;
- 
+    leagueName.textContent = nextLeague.name;
+    leagueCountry.textContent = nextLeague.country;
+    leagueImage.src = nextLeague.leagueLogo;
+    leagueFlag.src = nextLeague.flag;
+    leagueStandings1.textContent = nextLeague.team1;
+    leagueStandings2.textContent = nextLeague.team2;
+    leagueStandings3.textContent = nextLeague.team3;
+    leagueStandings4.textContent = nextLeague.team4;
+    leagueStandings5.textContent = nextLeague.team5;
+    leagueStandings6.textContent = nextLeague.team6;
+    leagueStandings7.textContent = nextLeague.team7;
+    leagueStandings8.textContent = nextLeague.team8;
+    leagueStandings9.textContent = nextLeague.team9;
+    leagueStandings10.textContent = nextLeague.team10;
+    leagueStandings11.textContent = nextLeague.team11;
+    leagueStandings12.textContent = nextLeague.team12;
+    leagueStandings13.textContent = nextLeague.team13;
+    leagueStandings14.textContent = nextLeague.team14;
+    leagueStandings15.textContent = nextLeague.team15;
+    leagueStandings16.textContent = nextLeague.team16;
+    leagueStandings17.textContent = nextLeague.team17;
+    leagueStandings18.textContent = nextLeague.team18;
+    leagueStandings19.textContent = nextLeague.team19;
+    leagueStandings20.textContent = nextLeague.team20;
 
+
+}
+function promoVideo(leagueData) {
+    let promoVideoSoccer = document.querySelector("iframe")
+    leagueData.forEach(video => {
+        promoVideoSoccer.promoVideo = video.src
+        promoVideoSoccer.append(promoVideoSoccer.promoVideo)
+
+    })
 }
 function addComment() {
     let newComment = document.querySelector("#comment-form")
@@ -113,7 +123,7 @@ function addComment() {
         event.preventDefault();
 
         const newCommentAdded = {
-            "content" : event.target["comment"].value
+            "content": event.target["comment"].value
         }
         const newSoccerComment = document.createElement("li");
         newSoccerComment.textContent = newCommentAdded.content;
@@ -122,14 +132,14 @@ function addComment() {
         addLeagues(newCommentAdded)
     })
 }
-function mouseOverFunction(){
+function mouseOverFunction() {
     mouseOver.addEventListener("mouseover", () => {
         document.getElementById("title").style.color = "red"
         let playAnthem = document.getElementById("controls")
 
     })
 }
-function mouseOutFunction(){
+function mouseOutFunction() {
     mouseOut.addEventListener("mouseout", () => {
         document.getElementById("title").style.color = "black"
     })
@@ -143,19 +153,19 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
 
 
